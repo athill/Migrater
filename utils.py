4032aafaf5a5348d8f9af3a593bb2d3aa0f595d7
Andy Hill <andy@andyhill.us>
2014-04-01 17:54:21 -0400

    Added utils. Not using yet

---
+++ utils.py
@@ -0,0 +1,16 @@
+import os
+
+
+# trying to bring jQuery.extend to Python
+def extend(self, defaults, opts):
+	"""Create a new dictionary with a's properties extended by b,
+	without overwriting.
+
+	>>> extend({'a':1,'b':2},{'b':3,'c':4})
+	{'a': 1, 'c': 4, 'b': 2}
+	http://stackoverflow.com/a/12697215
+	"""
+	return dict(defaults,**opts)
+
+def fixPath(self, path):
+	return path.replace('~', os.path.expanduser("~"), 1)
